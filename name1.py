31ba273c31928ed5017c86af915f492f2414a337
Robert <116393312+Robb-89@users.noreply.github.com>
2026-01-06 00:56:48 -0800

    Delete name1.py

--- name1.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-name = 'ada lovelace' 
-print(name.title())
-
-# The example explains that name is all lowercase 
-# the title function changes all letters to title case, so Ada Lovelace
-# .upper() ; .lower() either upper case all letters or lower case all letters. 
