0100e1e673af8c1787962b0aaf998bf174353eea
Robert <116393312+Robb-89@users.noreply.github.com>
2026-01-06 00:51:47 -0800

    Delete guest_list.py

--- guest_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-
-list_1 = ["alice","david","mary"]
-
-for list_1 in list_1:
-	print(f"Congrats {list_1.title()}, on winning the lottery!")
-	print(f"I hope you spend your money wisely!\n")
-	print(f"See you next time, goodbye!\n")
