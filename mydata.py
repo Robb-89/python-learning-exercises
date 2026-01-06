a811047852d14fe976a70d3af28b6b4ffcabd6e7
Robert <116393312+Robb-89@users.noreply.github.com>
2026-01-06 00:55:11 -0800

    Delete mydata.py

--- mydata.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-fruit = ["apple", "banana", "orange", "mango"]
-
-veg = ["carrot", "broccoli", "peas", "artichoke"]
-
-def printItems(myList):
-    for x in myList:
-        print(x)
