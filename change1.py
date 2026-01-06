305f55d58247df8cf30fe8f1c24a38ceae2e2c36
Robert <116393312+Robb-89@users.noreply.github.com>
2026-01-06 00:45:50 -0800

    Delete change1.py

--- change1.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-
-famous_person = "Eminem" 
-
-message = "The truth is you don't know what is going to happen tomorrow. Life is a crazy ride, and nothing is guaranteed. -Eminem"
-
-print(message)
