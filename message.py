b84ece2eaee9c8896bf031f8842d4b1d67521ec8
Robert <116393312+Robb-89@users.noreply.github.com>
2026-01-06 00:54:54 -0800

    Delete message.py

--- message.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-# This is a simple code. I write my message, and then print it to the screen when i run the script. I can possibly add user input later on. 
-message_1 = "Hello, I think i enjoy Python more than Bash scripting, but both are really useful in different manners. What do you think?"
-
-print(message_1)
